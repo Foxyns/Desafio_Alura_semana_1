bd945824784c8e3fe190c419678e1ff34923dcd4
Foxyns <joarles34@gmail.com>
2023-10-20 17:18:44 -0300

    completei todo mecanismo do botão criptografar

--- scripts/script_criptografar.js
+++ scripts/script_criptografar.js
@@ -1,13 +1,17 @@
 /* criar função que configura os valores das chaves que serão usadas */
+
+
 let button_criptografar = document.getElementById("button_criptografar");
-let text = document.querySelector("textarea")
-let text_cripto = ""
-let a = "ai"
-let e = "enter"
-let i = "imes"
-let o = "ober"
-let u = "ufat"
+let text = document.querySelector("textarea");
+let text_cripto = "";
+let a = "ai";
+let e = "enter";
+let i = "imes";
+let o = "ober";
+let u = "ufat";
 function criptografar(){
+   
+
     for (index=0; index<text.value.length; index++){
         if (text.value[index] == "a"){
             text_cripto += a;
@@ -23,7 +27,21 @@ function criptografar(){
             text_cripto += text.value[index];
         }
     }
-  document.write(text_cripto)  
+    // mais pra frente quero refatorar essa parte alguns sites uteis para isso: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model e https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Examples
+    
+    // tiro a imagem e o paragrafo do display
+    document.getElementById("img_output").style.display = "none";
+    document.getElementById("div_output_p").style.display = "none";
+
+    // estou criando um elemento <p> contendo o texto criptografado
+    let p_ = document.createElement("p");
+    let texto_formatado = document.createTextNode(text_cripto);
+    p_.appendChild(texto_formatado);
+    
+    
+    // aqui estou incluindo o elemento p com meu texto criptografado na minha div_output
+    document.getElementById("div_output").replaceChild(p_, document.getElementById("div_output").childNodes[0])
+    text_cripto = "";  
 }
 
 
