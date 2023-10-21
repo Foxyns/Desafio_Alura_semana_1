e1aa5df3418277bcbaeb85db0d3c4f06f67b0a82
Foxyns <joarles34@gmail.com>
2023-10-21 05:49:31 -0300

    finalizei o botão de descriptografia e adicionei o botão de copiar

--- scripts/script_criptografar.js
+++ scripts/script_criptografar.js
@@ -1,16 +1,46 @@
-/* criar função que configura os valores das chaves que serão usadas */
-
-
-let button_criptografar = document.getElementById("button_criptografar");
+// mais pra frente quero refatorar essa parte alguns sites uteis para isso: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model e https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Examples https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 let text = document.querySelector("textarea");
 let text_cripto = "";
-let text_criptografado = "";
+let text_descripto = ""
+let texto_copiar = ""
+let  p_ = "";
+
 
 let a = "ai";
 let e = "enter";
 let i = "imes";
 let o = "ober";
 let u = "ufat";
+
+
+
+let button_copiar = document.getElementById("button_copiar");
+function copiar(){ 
+    if(texto_copiar != ""){
+        navigator.clipboard.writeText(texto_copiar)
+        .then(() => {
+            alert("Copiado");
+        })
+                
+    } 
+}
+
+
+function limparImagem(){
+    document.getElementById("img_output").style.display = "none";
+    document.getElementById("div_output_p").style.display = "none";
+    
+}
+
+function mudarMensagem(texto){   
+    p_ = document.createElement("p");
+    p_.setAttribute("id", "texto_output")  
+    texto_formatado = document.createTextNode(texto);
+    p_.appendChild(texto_formatado); 
+    document.getElementById("div_output").replaceChild(p_, document.getElementById("div_output").childNodes[0])
+}
+
+
 function criptografar(){
    
 
@@ -29,53 +59,52 @@ function criptografar(){
             text_cripto += text.value[index];
         }
     }
-    // mais pra frente quero refatorar essa parte alguns sites uteis para isso: https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model e https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Examples https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
     
-    // tiro a imagem e o paragrafo do display
-    document.getElementById("img_output").style.display = "none";
-    document.getElementById("div_output_p").style.display = "none";
 
-    // estou criando um elemento <p> contendo o texto criptografado
-    let p_ = document.createElement("p");
-    let texto_formatado = document.createTextNode(text_cripto);
-    p_.appendChild(texto_formatado);
-    
-    
-    
-    // aqui estou substituindo o elemento filho dessa div pelo nosso elemento criado que contem o texto criptografado
-    document.getElementById("div_output").replaceChild(p_, document.getElementById("div_output").childNodes[0])
-   
+    if(text_cripto != ""){limparImagem()};
+    mudarMensagem(text_cripto);
+    texto_copiar = text_cripto;
     text_cripto = "";  
 
 }
 
-button_criptografar.onclick = criptografar;
 
 
-
-
-
-
-
-
-let button_descriptografar = document.getElementById("button_descriptografar");
 function descriptografar(){
-    while(text.value.includes("ai")){
-        console.log('oi')
+    for (index=0; index < text.value.length; index++){
+        if(text.value[index] == "a" && text.value[index+1] == "i"){
+            text_descripto += text.value[index]
+            index+=1
+        }else if(text.value[index] == "e" && text.value[index+1] == "n" && text.value[index+2] == "t" && text.value[index+3] == "e" && text.value[index+4] == "r"){
+            text_descripto += text.value[index]
+            index+=4
+
+        }else if(text.value[index] == "i" && text.value[index+1] == "m" && text.value[index+2] == "e" && text.value[index+3] == "s"){
+            text_descripto += text.value[index]
+            index+=3
+            
+        }else if(text.value[index] == "o" && text.value[index+1] == "b" && text.value[index+2] == "e" && text.value[index+3] == "r"){
+            text_descripto += text.value[index]
+            index+=3
+            
+        }else if(text.value[index] == "u" && text.value[index+1] == "f" && text.value[index+2] == "a" && text.value[index+3] == "t"){
+            text_descripto += text.value[index]
+            index+=3
+            
+        }else{
+            text_descripto += text.value[index]
+
+        }
 
     }
 
+    if(text_descripto != ""){limparImagem()}
 
-    //for (index=0; index < text.value.length; index++){
+    mudarMensagem(text_descripto);
 
-        //  console.log((text.value[i] == "a") && (text.value[i+1] == "i"));
-        //if((text.value.in == "a") && (text.value[i+1] == "i")){
-    
-         //  console.log((text.value.replace("ai", "a").replace("ober", "o").replace("enter", "e").replace("ims", "i").replace("ufat", "u")));
+    texto_copiar = text_descripto;
 
-      //  }
-      //  break;
-   // }
+    text_descripto = ""
 }
 
-button_descriptografar.onclick = descriptografar;
+
